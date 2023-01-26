6bf0ab8fa86f222d2df7c9c194318aa3b4ff9485
MostaphaEchelhi <mostapha.echelhi@medieinstitutet.se>
2023-01-26 17:23:22 +0100

    spara

---
+++ main.js
@@ -0,0 +1,14 @@
+const menuBtn = document.querySelector('.btnMenu');
+const navMenu = document.querySelector('#nav');
+
+menuBtn.addEventListener('click', openMenu);
+navMenu.addEventListener('click', closeMenu);
+
+function openMenu () {
+    navMenu.classList.toggle('.menu');
+}
+
+function closeMenu () {
+    navMenu.classList.toggle('.menu');
+}
+
